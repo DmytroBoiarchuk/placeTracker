072fd276d1602f206f3caeb4d3ce12dacd893de1
Dmytro <dimabwhite98@gmail.com>
2024-12-11 18:04:26 +0000

    card appearance animation added

--- src/Components/PlacesList/PlacesList.tsx
+++ src/Components/PlacesList/PlacesList.tsx
@@ -1,20 +1,33 @@
 import { useQueryClient } from "@tanstack/react-query";
 import classes from "./PlacesList.module.scss";
 import { CacheKeyContext } from "../../store/cacheKeyContext.tsx";
-import {useContext} from "react";
+import { useContext } from "react";
 import { SearchResultInterface } from "../../interfaces/interfaces.ts";
 import PlaceCard from "./Components/PlaceCard/PlaceCard.tsx";
+import { motion } from "motion/react";
 
 const PlacesList = (): JSX.Element => {
   const queryClient = useQueryClient();
   const cacheKeyCtx = useContext(CacheKeyContext);
 
   //retrieve query result from cache using keys we got from Context
-  const cachedData: SearchResultInterface | undefined = queryClient.getQueryData([
-    "search",
-    cacheKeyCtx.searchTerm,
-    cacheKeyCtx.coordinates,
-  ]);
+  const cachedData: SearchResultInterface | undefined =
+    queryClient.getQueryData([
+      "search",
+      cacheKeyCtx.searchTerm,
+      cacheKeyCtx.coordinates,
+    ]);
+  const listItemVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.2, // Задержка для каждого элемента
+        duration: 0.5,
+      },
+    }),
+  };
   return (
     <section className={classes.listSection}>
       {!cachedData && (
@@ -26,16 +39,22 @@ const PlacesList = (): JSX.Element => {
       {cachedData && (
         <ul className={classes.list}>
           {cachedData?.results
-            .sort((a, b) => b.rating !== undefined?  b.rating - a.rating: -1)
+            .sort((a, b) => (b.rating !== undefined ? b.rating - a.rating : -1))
             .filter(
               (p) =>
                 p.location.formatted_address !== undefined &&
                 p.location.address !== undefined,
             )
-            .map((place) => (
-              <li key={place.fsq_id}>
+            .map((place, index) => (
+              <motion.li
+                key={place.fsq_id}
+                custom={index}
+                initial="hidden"
+                animate="visible"
+                variants={listItemVariants}
+              >
                 <PlaceCard fsq_id={place.fsq_id} />
-              </li>
+              </motion.li>
             ))}
         </ul>
       )}
